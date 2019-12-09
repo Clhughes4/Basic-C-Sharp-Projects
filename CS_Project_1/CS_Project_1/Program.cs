d2b64082bba2cccb4c7c17c4e6cb2d5a20d04a59
Clint Hughes <cahughes27@gmail.com>
2019-12-09 12:26:45 -0800

    created Hello World
    created hello world project as CS_Project_1

---
+++ CS_Project_1/CS_Project_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace CS_Project_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Acme Accounting Systems");
+            Console.WriteLine("Remember, we are " + "accounting" + " on you!");
+            Console.Read();     
+        }
+    }
+}
