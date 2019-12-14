c59e4a2f0ec3f9ed1079ef4f4d07646d1b09ab08
Clint Hughes <cahughes27@gmail.com>
2019-12-14 06:24:03 -0800

    Create MethodDrill

---
+++ MethodDrill/MethodDrill/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill
+{
+    class Math
+    {
+        public int Multiply(int mulNum)
+        {
+            int num = 56;
+            int num1 = 34;
+            return mulNum * num * num1;
+        }
+
+        public int Divide(int addNum)
+        {
+            int num2 = 4;
+            int num3 = 3;
+            return addNum / num2 / num3;
+        }
+
+        public int Subtract(int subNum)
+        {
+            int num4 = 34;
+            int num5 = 9;
+            return  subNum - num4 - num5;
+        }
+    }
+}
---
+++ MethodDrill/MethodDrill/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bookMark:
+            try
+            {
+                
+                Console.WriteLine("Please choose a number:");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                Math math = new Math();
+                
+
+                Console.WriteLine("Your number ran through the Divide Method is = " + math.Divide(userInput) + "\n");
+
+                Console.WriteLine("Your number ran through the Subtract Method is = " + math.Multiply(userInput) + "\n");
+
+                Console.WriteLine("Your number ran the through Multiply Method is = " + math.Subtract(userInput) + "\n");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please choose a whole number."); // This catch will run even if it is an empty or null value
+                goto bookMark;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                goto bookMark;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
