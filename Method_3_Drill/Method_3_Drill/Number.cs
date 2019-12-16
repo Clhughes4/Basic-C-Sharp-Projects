eaff1f5a704e5ae5896dabad3a3b8f95aa537a5e
Clint Hughes <cahughes27@gmail.com>
2019-12-16 12:23:24 -0800

    Created Method_3_Drill

---
+++ Method_3_Drill/Method_3_Drill/Number.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_3_Drill
+{
+    class Number
+    {
+        public int Integar(int a)
+        {
+            int num = 50;
+            return  a + num;
+
+        }
+        public decimal Integar(decimal a)
+        {
+            decimal num = Convert.ToInt32(17);
+            return a * num;
+        }
+
+        public int Integar(string a)
+        {
+            int num = 406;
+            int newNum = Convert.ToInt32(a);
+            int div = num / newNum;
+            return div;
+        }
+
+    }
+}
---
+++ Method_3_Drill/Method_3_Drill/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_3_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number add = new Number();
+            Console.WriteLine(add.Integar(45));
+
+            Number dec = new Number();
+            decimal decNum = 34;
+            Console.WriteLine(dec.Integar(decNum));
+
+            Number str = new Number();
+            string strConvert = Convert.ToString(str.Integar("88"));
+            Console.WriteLine(strConvert);
+
+        }
+    }
+}
