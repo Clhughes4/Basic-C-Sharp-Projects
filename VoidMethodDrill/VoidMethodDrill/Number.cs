f268bda0030d21040dd7097eb405523aefc6c347
Clint Hughes <cahughes27@gmail.com>
2019-12-16 12:21:45 -0800

    Create VoidMethodDrill

---
+++ VoidMethodDrill/VoidMethodDrill/Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodDrill
+{
+    class Number
+    {
+        public void AddNum(int num1,int num2)
+        {
+            
+            int add = num1 + 100;
+            Console.WriteLine(num2);
+            Console.WriteLine(add);
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
---
+++ VoidMethodDrill/VoidMethodDrill/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.AddNum(56, 78);
+
+            number.AddNum(num1: 36, num2: 44);
+        }
+    }
+}
