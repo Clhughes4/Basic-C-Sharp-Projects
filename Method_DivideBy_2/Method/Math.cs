3f019e6e3827fc86c5b7e974d410cb09474e2d7f
Clint Hughes <cahughes27@gmail.com>
2019-12-16 17:20:00 -0800

    Created Method_DivideBy_2 project

---
+++ Method_DivideBy_2/Method/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Math
+    {
+        public void Divide(int num)
+        {
+            int answer = num / 2;
+            Console.WriteLine("The answer is: {0}\n", answer);
+        }
+
+        public decimal Multiply(decimal num, decimal num1)
+        {
+            decimal result = num * num1;
+            return result;
+        }
+
+        public int Multiply(int num)
+        {
+            int a = 23;
+            int result = num * a;
+
+            return result;
+        }
+    }
+}
---
+++ Method_DivideBy_2/Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+   static class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Enter a number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            math.Divide(input);
+
+            Console.WriteLine(" 77 * 32 = " + math.Multiply(77, 32)+ "\n");
+            Console.WriteLine(" 45 * 23 = " + math.Multiply(45) + "\n");
+        }
+    }
+}
