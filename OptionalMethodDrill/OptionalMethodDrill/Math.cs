b13b259be0e242fa616bec18f5cc2ae875dff45a
Clint Hughes <cahughes27@gmail.com>
2019-12-16 16:25:13 -0800

    Create OptionalMethodDrill

---
+++ OptionalMethodDrill/OptionalMethodDrill/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalMethodDrill
+{
+    class Math
+    {
+        public int Add(int num, out int number, int num1 = 7)
+        {
+            int result = (num + num1) * 86;
+            number = 0;
+
+            return result;
+        }
+    }
+}
---
+++ OptionalMethodDrill/OptionalMethodDrill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalMethodDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input two numbers, or choose only one number:\n" +
+                "Enter your first number.\n");
+            int input = Convert.ToInt32(Console.ReadLine() + "\n");
+            Math math = new Math();
+            bookMark:
+            try
+            {
+                Console.WriteLine("Now enter you second number:\n");
+                int input2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine(math.Add(input, out input2) + "\n");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please choose a whole number or enter a zero to no number.");
+                goto bookMark;
+            }
+
+        }
+    }
+}
