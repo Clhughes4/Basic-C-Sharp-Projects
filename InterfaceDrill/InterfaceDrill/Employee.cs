4c9534337e94f2c868e1ec9a5db6e70b76a7993c
Clint Hughes <cahughes27@gmail.com>
2019-12-17 14:32:15 -0800

    Create InterfaceDrill Project

---
+++ InterfaceDrill/InterfaceDrill/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDrill
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("The Student is the: \n\n  {0} {1}", firstName, lastName);
+
+        }
+        public void Quit()
+        {
+            Console.WriteLine("This method was succesful printing to the console.\n");
+
+        }
+    }
+}
---
+++ InterfaceDrill/InterfaceDrill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable person = new Employee();
+            person.Quit();
+            ((Employee)person).firstName = "Gabe";
+            ((Employee)person).lastName = "Hughes";
+            ((Employee)person).SayName();
+        }
+    }
+}
