248ac6acd2e28d89053cd6146fb93ceed322596a
Clint Hughes <cahughes27@gmail.com>
2019-12-18 14:58:56 -0800

    Create OverloadOperatorDrill Project

---
+++ OverloadOperatorDrill/OverloadOperatorDrill/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperatorDrill
+{
+    public class Employee: Person
+    {
+        public int ID { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            //bool status = false;
+            if (employee1.ID == employee2.ID)
+            {
+                //status = true;
+            }
+            return (employee1 == employee2);
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool status = false;
+            if (employee1.ID != employee2.ID)
+            {
+                status = true;
+            }
+            return !(employee1 == employee2);
+        }
+    }
+
+
+}
---
+++ OverloadOperatorDrill/OverloadOperatorDrill/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperatorDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Leonidis";
+            employee.LastName = "Pascoli";
+            employee.ID = 94175;
+
+            Employee employee2 = new Employee();
+            employee2.FirstName = "Jacobee";
+            employee2.LastName = "Damascus";
+            employee2.ID = 94179;
+
+            
+            Console.WriteLine(employee != employee2);
+                
+
+        }
+       
+    }   
+}
