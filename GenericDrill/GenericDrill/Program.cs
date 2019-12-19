bd4a94d51f71bf324d05cb41a07a61ab9586850e
Clint Hughes <cahughes27@gmail.com>
2019-12-18 16:59:42 -0800

    Created GenericDrill

---
+++ GenericDrill/GenericDrill/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string> { "Clint", "Gabe", "Jaxson", "Dexley" };
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(employee.Things[i] );
+            }
+            Console.WriteLine();
+            Employee<int> number = new Employee<int>();
+            number.Things = new List<int> { 8, 4, 7, 3, 9, 2 };
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(number.Things[i]);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
