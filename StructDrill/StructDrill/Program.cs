3e760ae80922b247c23a1b62bf7784a2a24ad74b
Clint Hughes <cahughes27@gmail.com>
2019-12-20 06:07:01 -0800

    Create StructDrill

---
+++ StructDrill/StructDrill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 84;
+            Number number2 = new Number();
+            number2.Amount = 36;
+            Console.WriteLine("The amount is: {0} and {1}", number.Amount, number2.Amount);
+        }
+    }
+}
