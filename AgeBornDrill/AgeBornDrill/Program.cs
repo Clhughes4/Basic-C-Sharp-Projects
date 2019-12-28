362fa8c137967b182503afb58e7b024e16cac48b
Clint Hughes <cahughes27@gmail.com>
2019-12-28 12:24:09 -0800

    Created Constant Var Drill & AgeBornDrill

---
+++ AgeBornDrill/AgeBornDrill/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeBornDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bookMark:
+            try
+            {
+                Console.WriteLine("What is your age?");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                if (age < 0 || age == 0 || age > 100 )
+                {
+                    while (age < 0 || age == 0 || age > 100)
+                    {
+                        Console.WriteLine("Please enter a valid age");
+                        break;
+                    }
+                        goto bookMark;
+
+                }
+                else
+                {
+                    var today = DateTime.Today;
+                    var yearBorn = today.Year - age;
+                    Console.WriteLine("You were born in {0}", yearBorn);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number");
+                //return;
+                goto bookMark;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //return;
+                goto bookMark;
+            }
+            
+
+        }
+
+    }
+}
---
+++ Constant_VarDrill/Constant_VarDrill/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant_VarDrill
+{
+    class Person
+    {
+        private const string place = "New";
+        private string name;
+        private int id;
+       
+        public Person (int id) :this(id, "")
+        {
+        }
+        public Person (int id, string name) 
+        {
+            var person = "John";
+            this.id = id;
+            this.name = name;
+        }
+       
+    }
+}
